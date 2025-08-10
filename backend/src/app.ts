e93be96e447d59d9523576d435813ef89f7fd145
platovich <platovich@yandex.ru>
2025-08-10 17:44:18 +0300

    feat: add basic connection between front and backend.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,21 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+// CORS configuration
+const corsOptions = {
+  origin: FRONTEND_URL, // Adjust this to your frontend URL
+};
 
 // Middleware
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to QuickFixAI");
+});
 
 export default app;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const URL = import.meta.env.VITE_BACKEND_URL;
+  const [data, setData] = useState<string>("");
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(URL);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const text = await response.text();
+        setData(text);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{data}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
