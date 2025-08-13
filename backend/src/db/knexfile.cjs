86cd484c0e50a02e69e1db7d5dc6aed519c5fb0d
platovich <platovich@yandex.ru>
2025-08-13 13:57:23 +0300

    feature: add knexfile for config database

---
+++ backend/src/db/knexfile.cjs
@@ -0,0 +1,50 @@
+console.log("Knexfile is being executed");
+const path = require("path");
+
+console.log(path.join(__dirname, "./config/migrations"));
+const DATABASE_URL = process.env.DATABASE_URL;
+const NODE_ENV = process.env.NODE_ENV || "development";
+console.log("Database URL:", DATABASE_URL);
+console.log("NODE_ENV:", NODE_ENV);
+
+const config = {
+  development: {
+    client: "pg",
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "../config/migrations"),
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: path.join(__dirname, "../config/seeds"),
+    },
+  },
+  production: {
+    client: "pg",
+    connection: DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: path.join(__dirname, "../config/migrations"),
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: path.join(__dirname, "../config/seeds"),
+    },
+  },
+  test: {
+    client: "pg",
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "../config/migrations"),
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: path.join(__dirname, "../config/seeds"),
+    },
+  },
+};
+
+module.exports = config[NODE_ENV] || config.development;
