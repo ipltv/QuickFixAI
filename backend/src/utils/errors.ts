d7f734cc07822650f074ea5e4222a73342794bf0
platovich <platovich@yandex.ru>
2025-08-17 01:17:42 +0300

    feat: connect errorHandler to backend

--- backend/src/utils/errors.ts
+++ backend/src/utils/errors.ts
@@ -11,7 +11,7 @@ export class APIError extends Error {
   constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
-    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    // Maintains proper stack trace
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, this.constructor);
     }
