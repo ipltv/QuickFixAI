54e89cd04f3b625e15bfeb1e6549123e7d261d68
platovich <platovich@yandex.ru>
2025-08-22 15:13:34 +0300

    feat: add user slice with CRUD operations, edit store

--- frontend/src/types/user/user.ts
+++ frontend/src/types/user/user.ts
@@ -1,4 +1,5 @@
 import type { Role } from "../misc/roles.js";
+import type { RequestStatus } from "../misc/requestStatuses.js";
 
 // Define the User type with required fields
 export interface User {
@@ -7,4 +8,27 @@ export interface User {
   name: string;
   role: Role;
   clientId: string;
+}
+
+export interface NewUserPayload {
+  email: string;
+  password: string;
+  name: string;
+  role: Role;
+  clientId: string;
+}
+
+export interface UserUpdatePayload {
+  email?: string;
+  password?: string;
+  name?: string;
+  role?: Role;
+  clientId?: string;
+}
+
+// --- Redux State Shape for the User Feature ---
+export interface UserState {
+  users: User[];
+  status: RequestStatus;
+  error: string | null;
 }
