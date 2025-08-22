bb986749fc8c05e8933d310ac4c5c987e9ef620a
Ilya Platovich <49819033+ipltv@users.noreply.github.com>
2025-08-22 17:08:17 +0300

    Revert "Feature/user management"

--- frontend/src/types/user/user.ts
+++ frontend/src/types/user/user.ts
@@ -1,5 +1,4 @@
 import type { Role } from "../misc/roles.js";
-import type { RequestStatus } from "../misc/requestStatuses.js";
 
 // Define the User type with required fields
 export interface User {
@@ -7,28 +6,5 @@ export interface User {
   email: string;
   name: string;
   role: Role;
-  client_id: string;
-}
-
-export interface NewUserPayload {
-  email: string;
-  password: string;
-  name: string;
-  role: Role;
-  client_id: string;
-}
-
-export interface UserUpdatePayload {
-  email?: string;
-  password?: string;
-  name?: string;
-  role?: Role;
-  client_id?: string;
-}
-
-// --- Redux State Shape for the User Feature ---
-export interface UserState {
-  users: User[];
-  status: RequestStatus;
-  error: string | null;
+  clientId: string;
 }
